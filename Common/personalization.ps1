206fc9960a7799461db98f6ad285b7215ef04fcd
Kenneth Fechter <kenneth.fechter@gmail.com>
2020-09-18 23:22:55 -0400

    Removing prompt bit as it breaks powershell

--- Common/personalization.ps1
+++ Common/personalization.ps1
@@ -2,12 +2,3 @@
 if ([bool]($env:WT_Session)) {
     Set-Theme Paradox
 }
-else {
-    function prompt {
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSProvideCommentHelp', '', Scope = 'Function')]
-        param()
-        Write-Output "Previous Command: " -NoNewline
-        Write-Output "$((Get-Date).ToUniversalTime()) (UTC)" -NoNewline -ForegroundColor Yellow
-        "$(Get-Location)>"
-    }
-}
