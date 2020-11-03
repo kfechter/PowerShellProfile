ea58cb45a12a74275f98822e89a53d96771bcdb1
Kenneth Fechter <kenneth.fechter@gmail.com>
2020-11-02 23:00:16 -0500

    Dynamic param for project autocomplete

--- Common/aliases.ps1
+++ Common/aliases.ps1
@@ -27,6 +27,29 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
 }
 
 if ($null -ne $env:GIT_PROJECT_ROOT_PATH) {
+
+    function Show-Projects {
+        <#
+.SYNOPSIS
+Shows list of folder names in GIT_PROJECT_ROOT_PATH
+ .DESCRIPTION
+if GIT_PROJECT_ROOT_PATH environment variable is set, this function allows the user to
+quickly list all projects in the project root
+
+
+.EXAMPLE
+PS> Show-Projects
+#>
+        $Projects = (Get-ChildItem -Path $env:GIT_PROJECT_ROOT_PATH).Name
+
+        if ($Projects) {
+            $Projects
+        }
+        else {
+            Write-Warning "No projects to show."
+        }
+    }
+
     function Start-Work {
         <#
 .SYNOPSIS
@@ -42,18 +65,38 @@ The name of the project to push-location into the root of.
 PS> Start-Work MyProject
 "Setting Location to GIT_PROJECT_ROOT_URL\MyProject"
 #>
+        [CmdletBinding()]
+        Param()
 
-        Param(
-            [Parameter(Mandatory = $true)][string]$ProjectName
-        )
+        DynamicParam {
+            $attributes = new-object System.Management.Automation.ParameterAttribute
+            $attributes.Mandatory = $true
 
-        $ProjectPath = "$($env:GIT_PROJECT_ROOT_PATH)\$ProjectName"
-        if (Test-Path -Path $ProjectPath) {
-            Write-Output -InputObject "Switching Location to $ProjectPath"
-            Push-Location -Path $ProjectPath
+            $attributeCollection = new-object -Type System.Collections.ObjectModel.Collection[System.Attribute]
+            $attributeCollection.Add($attributes)
+
+            $validateSet = Show-Projects
+            if ($validateSet.count -ne 0) {
+                $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($validateSet)
+                $AttributeCollection.Add($ValidateSetAttribute)
+            }
+
+            $dynamicParameter = new-object -Type System.Management.Automation.RuntimeDefinedParameter("ProjectName", [string], $attributeCollection)
+            $paramDictionary = new-object -Type System.Management.Automation.RuntimeDefinedParameterDictionary
+            $paramDictionary.Add("ProjectName", $dynamicParameter)
+
+            return $paramDictionary
         }
-        else {
-            Write-Warning -Message "$ProjectPath does not exist"
+
+        End {
+            $ProjectPath = "$($env:GIT_PROJECT_ROOT_PATH)\$($PSBoundParameters['ProjectName'])"
+            if (Test-Path -Path $ProjectPath) {
+                Write-Output -InputObject "Switching Location to $ProjectPath"
+                Push-Location -Path $ProjectPath
+            }
+            else {
+                Write-Warning -Message "$ProjectPath does not exist"
+            }
         }
     }
 
